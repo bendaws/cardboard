10e5e182ec9cfdc4c6717f3ebfc0cde3dfead7a0
Ben <ben@bendaws.net>
2024-11-18 20:44:57 +0000

    tools: try and fix action

--- utils/translate-rblx.py
+++ utils/translate-rblx.py
@@ -6,8 +6,10 @@ definitions = [
 ]
 
 for file in files_to_translate:
-    with open(file, "w") as file_content:
-        for line in file_content.readlines():
+    with open(file, "r") as file_content:
+        modify_content = file_content
+
+        for line in modify_content.readlines():
             if str.find(line, "require"):
                 location = str.find(line, "require")
 
@@ -15,5 +17,6 @@ for file in files_to_translate:
                     if str.find(line, definition[0]):
                         str.replace(line, definition[0], definition[1])
         
-        file_content.writelines(file_content)
-        file_content.close()
+        with open(file, "w") as file_write:
+            file_content.writelines(modify_content)
+            file_content.close()
